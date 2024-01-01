fcc2dfe9fe128b8ffeeeb78e876a537db9c1db53
Tarun Rai <tarunraikhs1122@gmail.com>
2024-01-01 14:46:25 +0530

    Form Post With Localhost Server with id password

--- index.js
+++ index.js
@@ -7,17 +7,47 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+
+let userIsAuthorised = false;
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname));
 
+function passwordCheck(req, res, next) {
+  const username = req.body.username;
+  const pass = req.body.pass; // Change variable name to pass
+  if (username === "123456" && pass === "login") {
+    userIsAuthorised = true;
+  }
+  next();
+}
 
+app.set("view engine", "ejs");
+app.use(passwordCheck);
 app.set();
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
 });
 
 
+app.post("/", (req, res) => {
+  if (userIsAuthorised == true) {
+  res.sendFile(path.join(__dirname, "superadmindashboard.html"));
+
+    // res.send("<h1>Welcome User!</h1>");
+  } else {
+    res.sendFile(path.join(__dirname, "loginfailed.html"));
+
+  }
+});
+
 app.listen(3000, () => {
   console.log('Your server is started..');
 });
+
+
+
+
+
